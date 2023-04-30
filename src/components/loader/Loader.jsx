73fd12fe244541342544422884eeb0dd832b0cd3
Валентина Сіра <113180300+ValentynaSira1@users.noreply.github.com>
2023-04-30 23:09:29 +0300

    add

---
+++ src/components/loader/Loader.jsx
@@ -0,0 +1,15 @@
+import { Triangle } from 'react-loader-spinner';
+
+export const Loader = () => {
+    return (
+        <Triangle
+          height="80"
+          width="100%"
+          color="#3f51b5"
+          ariaLabel="triangle-loading"
+          wrapperStyle={{}}
+          wrapperClassName=""
+          visible={true}
+        />
+      )
+}
